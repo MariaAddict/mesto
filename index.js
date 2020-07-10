b98da6c5005984231a4dfe6d8cca30262230b7eb
MariaAddict <mashakan27@gmail.com>
2020-07-10 15:41:59 +0700

    refactor: блок form изменить на элемент блока mpdal и остальные состовляющие блока form

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let modal = document.querySelector('.modal');
 let closeModalButton = document.querySelector('.modal__close-button');
 let nameProfile = document.querySelector('.profile__name');
 let activityProfile = document.querySelector('.profile__activity-type');
-let editName = document.querySelector('.form__item_name_name');
-let editActivity = document.querySelector('.form__item_name_activity-type');
-let form = modal.querySelector('.form');
+let editName = document.querySelector('.modal__item_name_name');
+let editActivity = document.querySelector('.modal__item_name_activity-type');
+let form = modal.querySelector('.modal__form');
 
 editName.value = nameProfile.textContent;
 editActivity.value = activityProfile.textContent;
