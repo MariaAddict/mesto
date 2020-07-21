7114095f8545350e5dba42f132e5c7675016a545
MariaAddict <mashakan27@gmail.com>
2020-07-21 18:28:01 +0700

    feat: добавить попап для добавления карточек, открытие, закрытие попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,34 @@
-let openModalButton = document.querySelector('.profile__info-button');
 let modal = document.querySelector('.modal');
-let closeModalButton = document.querySelector('.modal__close-button');
+let modalEdit = document.querySelector('.modal_type_edit');
+let modalAdd = document.querySelector('.modal_type_add');
+let formEdit = modalEdit.querySelector('.modal__form');
+let formAdd = modalAdd.querySelector('.modal__form');
+let openModalEditButton = document.querySelector('.profile__info-button');
+let openModalAddButton = document.querySelector('.profile__add-button');
+let closeModalEditButton = modalEdit.querySelector('.modal__close-button');
+let closeModalAddButton = modalAdd.querySelector('.modal__close-button');
 let nameProfile = document.querySelector('.profile__name');
 let activityProfile = document.querySelector('.profile__activity-type');
 let editName = document.querySelector('.modal__item_name_name');
 let editActivity = document.querySelector('.modal__item_name_activity-type');
-let form = modal.querySelector('.modal__form');
 
-function toggleButton () {
-    modal.classList.toggle('modal_opened');
-    if (modal.classList.contains('modal_opened') === true) {
-    editName.value = nameProfile.textContent;
-    editActivity.value = activityProfile.textContent;
-}
-}
+const toggleModal = (modal) => {
+  modal.classList.toggle('modal_opened');
+};
 
 function editInput () {
-    event.preventDefault();
-    nameProfile.textContent = editName.value;
-    activityProfile.textContent = editActivity.value;
-    toggleButton ();
+  event.preventDefault();
+  nameProfile.textContent = editName.value;
+  activityProfile.textContent = editActivity.value;
+  toggleModal(modalEdit);
 }
 
-openModalButton.addEventListener('click', toggleButton);
-closeModalButton.addEventListener('click', toggleButton);
-form.addEventListener('submit', editInput);
+openModalEditButton.addEventListener('click', () => { 
+  toggleModal(modalEdit);
+  editName.value = nameProfile.textContent;
+  editActivity.value = activityProfile.textContent;
+ });
+openModalAddButton.addEventListener('click', () => { toggleModal(modalAdd); });
+closeModalEditButton.addEventListener('click',  () => { toggleModal(modalEdit); });
+closeModalAddButton.addEventListener('click',  () => { toggleModal(modalAdd); });
+formEdit.addEventListener('submit', () => { editInput(modalEdit) });
