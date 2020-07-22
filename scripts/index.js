040fb6cd82041a45908ef6167fcc06806aaf64a1
MariaAddict <mashakan27@gmail.com>
2020-07-22 23:36:50 +0700

    refactor: добавить карточки через массив и добавить кнопку удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let modal = document.querySelector('.modal');
 let modalEdit = document.querySelector('.modal_type_edit');
 let modalAdd = document.querySelector('.modal_type_add');
@@ -22,6 +49,23 @@ function editInput () {
   activityProfile.textContent = editActivity.value;
   toggleModal(modalEdit);
 }
+
+const cardTemplate = document.querySelector('.card-template').content.querySelector('.cards__item');
+
+initialCards.forEach((data) => {
+  
+  const cardItem = cardTemplate.cloneNode(true);
+  const cardImage = cardItem.querySelector('.cards__image');
+  const cardTitle = cardItem.querySelector('.cards__title');
+  const cardLikeButton = cardItem.querySelector('.cards__like');
+  const cardDeleteButton = cardItem.querySelector('.cards__delete');
+  const cardsList = document.querySelector('.cards');
+
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardsList.append(cardItem);
+});
 
 openModalEditButton.addEventListener('click', () => { 
   toggleModal(modalEdit);
