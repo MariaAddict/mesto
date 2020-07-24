f053b2b03d3710c858130a95a649a22174b13fbd
MariaAddict <mashakan27@gmail.com>
2020-07-24 14:43:04 +0700

    refactor: упорядочить код JS, переменные изменить на const, fix: убрать нулевые значения при закрытии ImgModal

--- scripts/index.js
+++ scripts/index.js
@@ -25,19 +25,22 @@ const initialCards = [
   }
 ];
 
-let modal = document.querySelector('.modal');
-let modalEdit = document.querySelector('.modal_type_edit');
-let modalAdd = document.querySelector('.modal_type_add');
+const modal = document.querySelector('.modal');
+const modalEdit = document.querySelector('.modal_type_edit');
+const modalAdd = document.querySelector('.modal_type_add');
 const modalImage = document.querySelector('.modal_type_figure');
-let formEdit = modalEdit.querySelector('.modal__form');
-let formAdd = modalAdd.querySelector('.modal__form');
-let openModalEditButton = document.querySelector('.profile__info-button');
-let openModalAddButton = document.querySelector('.profile__add-button');
-let closeModalEditButton = modalEdit.querySelector('.modal__close-button');
-let closeModalAddButton = modalAdd.querySelector('.modal__close-button');
-const closeModalImageButton = modalImage.querySelector('.modal__close-button');
-let nameProfile = document.querySelector('.profile__name');
 
+const formEdit = modalEdit.querySelector('.modal__form');
+const formAdd = modalAdd.querySelector('.modal__form');
+
+const openModalEditButton = document.querySelector('.profile__info-button');
+const openModalAddButton = document.querySelector('.profile__add-button');
+
+const closeModalEditButton = modalEdit.querySelector('.modal__close-button');
+const closeModalAddButton = modalAdd.querySelector('.modal__close-button');
+const closeModalImageButton = modalImage.querySelector('.modal__close-button');
+
+let nameProfile = document.querySelector('.profile__name');
 let activityProfile = document.querySelector('.profile__activity-type');
 let editName = document.querySelector('.modal__item_type_name');
 let editActivity = document.querySelector('.modal__item_type_activity-type');
@@ -47,10 +50,7 @@ let figureImgModal =  modalImage.querySelector('.modal__image');
 let captionImgModal = modalImage.querySelector('.modal__caption');
 
 const cardsList = document.querySelector('.cards');
-
 const cardTemplate = document.querySelector('.card-template').content.querySelector('.cards__item');
-
-
 
 const toggleModal = (modal) => {
   modal.classList.toggle('modal_opened');
@@ -81,7 +81,6 @@ function createCard(card) {
     event.target.classList.toggle('card__like_pressed');
   })
 
-  
   cardImage.addEventListener('click', (event) => {
     figureImgModal.src = card.link;
     figureImgModal.alt = card.name;
@@ -102,32 +101,27 @@ function addCard() {
   toggleModal(modalAdd);
 };
 
+initialCards.forEach((data) => {
+  renderCard(data);
+});
+
 openModalEditButton.addEventListener('click', () => { 
   toggleModal(modalEdit);
   editName.value = nameProfile.textContent;
   editActivity.value = activityProfile.textContent;
  });
+
 openModalAddButton.addEventListener('click', () => { 
   toggleModal(modalAdd); 
   headerImage.value = '';
   urlImage.value = '';
 });
+
 closeModalEditButton.addEventListener('click',  () => { toggleModal(modalEdit); });
 closeModalAddButton.addEventListener('click',  () => { toggleModal(modalAdd); });
-closeModalImageButton.addEventListener('click',  () => { 
-  toggleModal(modalImage);
-  figureImgModal.src = "";
-  figureImgModal.alt = "";
-  captionImgModal.textContent = "";
-});
+closeModalImageButton.addEventListener('click',  () => {toggleModal(modalImage);});
+
 formEdit.addEventListener('submit', () => { editInput(modalEdit); });
-
-initialCards.forEach((data) => {
-  renderCard(data);
-});
-
-
-
 formAdd.addEventListener('submit', () => { 
   event.preventDefault();
   headerImage.textContent = '';
