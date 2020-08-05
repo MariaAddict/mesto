863f65e52916da5924a4aade8bd6547d588c7098
MariaAddict <mashakan27@gmail.com>
2020-08-05 18:12:41 +0700

    feat: добавить валидацию полей ввода

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,35 @@
-// включение валидации вызовом enableValidation
-// все настройки передаются при вызове
+/* formSelector, inputSelector, submitButtonSelector,inactiveButtonClass, inputErrorClass, errorClass */
+const enableValidation = ({formSelector, inputSelector, submitButtonSelector,inactiveButtonClass, inputErrorClass, errorClass}) => {
+    const formList = Array.from(document.querySelectorAll(formSelector));
+    formList.forEach((formElement) => {
+      formElement.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+      });
+      setEventListeners(formElement, inputSelector, submitButtonSelector, inputErrorClass);
+    });    
+ }
 
-enableValidation({
+  function setEventListeners(formElement, inputSelector, submitButtonSelector, inputErrorClass) {
+      const inputList = formElement.querySelectorAll(inputSelector);
+      const submitButton = formElement.querySelector(submitButtonSelector);
+      inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', () => {
+            
+            if (!inputElement.validity.valid) {
+                inputElement.classList.add(inputErrorClass);
+                hideInputError(formElement, inputElement)
+            } else {
+                inputElement.classList.remove(inputErrorClass);
+            }
+        });
+      });
+  }
+
+  function hideInputError(formElement, inputElement) {
+      const errorElement = formElement.querySelector(`#`);
+  }
+
+  enableValidation({
     formSelector: '.modal__form',
     inputSelector: '.modal__item',
     submitButtonSelector: '.modal__save-button',
