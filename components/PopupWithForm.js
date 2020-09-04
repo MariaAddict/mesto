b00d0d1c067155e3b631f8fff197b2044a090809
MariaAddict <mashakan27@gmail.com>
2020-09-04 15:49:23 +0700

    refactor: добавить класс PopupWithImage

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,1 +1,17 @@
-class PopupWithForm {}
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+    constructor(data, popupSelector) {
+        this._title = data.title;
+        this._url = data.url;
+        this.super(popupSelector);
+    }
+    open(){
+        figureImgModal.src = data.url;
+        figureImgModal.alt = this._title;
+        captionImgModal.textContent = this._title;
+        super.open();
+    }
+    
+    super(close);
+}
