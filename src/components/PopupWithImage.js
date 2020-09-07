3adbe3a8b34da49a6ed47ec92fe9c57e80f56588
MariaAddict <mashakan27@gmail.com>
2020-09-07 15:44:09 +0700

    refactor: добавить переменные в конструктор класса PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -5,12 +5,14 @@ export default class PopupWithImage extends Popup {
         super(popupSelector);
         this._name = data.name;
         this._link = data.link;
+        this._image = this._popup.querySelector('.modal__image');
+        this._caption = this._popup.querySelector('.modal__caption');
     }
 
     open() {
-        this._popup.querySelector('.modal__image').src = this._link;
-        this._popup.querySelector('.modal__image').alt = this._name;
-        this._popup.querySelector('.modal__caption').textContent = this._name;
+        this._image.src = this._link;
+        this._image.alt = this._name;
+        this._caption.textContent = this._name;
         super.open();
     }
 
